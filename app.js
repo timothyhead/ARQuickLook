703e2b20c3259b6af33a92535b507ee879eaef4b
Timtothy head <head.timothy3@googlemail.com>
2022-07-29 14:42:53 +0100

    added window open in app.js

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ const linkElement2 = document.getElementById("ar-link");
 linkElement.addEventListener("message", function (event) {
     if (event.data == "_apple_ar_quicklook_button_tapped") {
         // Handle the user tap.
-        console.log("ok");
+      window.open("https://www.w3schools.com");
     }
 }, false);

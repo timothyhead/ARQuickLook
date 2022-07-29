c23618b68d55f8782a214eec2f2cf5b6d390db06
Timtothy head <head.timothy3@googlemail.com>
2022-07-29 11:20:47 +0100

    added apple pay button

--- app.js
+++ app.js
@@ -7,3 +7,13 @@ linkElement.addEventListener("click", function (event) {
         console.log("Ok");
     }
 }, false);
+
+const linkElement2 = document.getElementById("ar-link");
+linkElement.addEventListener("message", function (event) { ... }, false);
+
+const linkElement = document.getElementById("ar-link");
+linkElement.addEventListener("message", function (event) {
+    if (event.data == "_apple_ar_quicklook_button_tapped") {
+        // Handle the user tap.
+    }
+}, false);

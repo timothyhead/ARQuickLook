7062b3264df7f8c6afd87160c9a4131ce146d326
Timtothy head <head.timothy3@googlemail.com>
2022-07-29 15:24:54 +0100

    changed evetListener to back and put window open above if satement

--- app.js
+++ app.js
@@ -2,8 +2,9 @@
 
 
 const linkElement = document.getElementById("ar-link");
-linkElement.addEventListener("click", function (event) {
-    if (event.target.value == "ar-link") {
+linkElement.addEventListener("message", function (event) {
+  window.open("https://www.w3schools.com");
+    if (event.data == "_apple_ar_quicklook_button_tapped") {
         // Handle the user tap.
         window.open("https://www.w3schools.com");
     }

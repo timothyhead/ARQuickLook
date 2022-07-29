62aa1012f9a13bca9162df1d05fcb49ef8c9b1a0
Timtothy head <head.timothy3@googlemail.com>
2022-07-29 13:43:28 +0100

    performed small delet in app.js

--- app.js
+++ app.js
@@ -1,12 +1,4 @@
-const linkElement = document.getElementById("ar-button");
 
-linkElement.addEventListener("click", function (event) {
-
-    if (event.target.value == "ar-button") {
-        // Handle the user tap.
-        console.log("Ok");
-    }
-}, false);
 
 const linkElement2 = document.getElementById("ar-link");
 linkElement.addEventListener("message", function (event) { ... }, false);
@@ -15,5 +7,6 @@ const linkElement2 = document.getElementById("ar-link");
 linkElement.addEventListener("message", function (event) {
     if (event.data == "_apple_ar_quicklook_button_tapped") {
         // Handle the user tap.
+        console.log("ok");
     }
 }, false);

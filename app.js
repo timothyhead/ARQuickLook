25f847dd5252ad8b03baf6a66543213e1ba2f29b
Timtothy head <head.timothy3@googlemail.com>
2022-07-29 15:29:21 +0100

    commented out open window above if statement

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 
 const linkElement = document.getElementById("ar-link");
 linkElement.addEventListener("message", function (event) {
-  window.open("https://www.w3schools.com");
+  // window.open("https://www.w3schools.com");
     if (event.data == "_apple_ar_quicklook_button_tapped") {
         // Handle the user tap.
         window.open("https://www.w3schools.com");

9279b443365d2fcc5a2ae896ab8b9456b5e5fb61
Timtothy head <head.timothy3@googlemail.com>
2022-07-29 15:06:54 +0100

    changed ecent.data to event.target.value and updated <a> tag accordingly

--- app.js
+++ app.js
@@ -3,8 +3,17 @@
 
 const linkElement = document.getElementById("ar-link");
 linkElement.addEventListener("message", function (event) {
-    if (event.data == "_apple_ar_quicklook_button_tapped") {
+    if (event.target.value == "ar-link") {
         // Handle the user tap.
+        window.open("https://www.w3schools.com");
+    }
+}, false);
+
+const linkElement2 = document.getElementById("ar-button");
+linkElement2.addEventListener("click", function (event) {
+    if (event.target.value== "button") {
+        // Handle the user tap.
+      console.log("Ok")
         window.open("https://www.w3schools.com");
     }
 }, false);

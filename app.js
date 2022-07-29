0508db85e40406bf37e058800078740406f6077d
Timtothy head <head.timothy3@googlemail.com>
2022-07-29 17:08:34 +0100

    small update

--- app.js
+++ app.js
@@ -9,12 +9,3 @@ linkElement.addEventListener("message", function (event) {
         window.open("https://www.w3schools.com");
     }
 }, false);
-
-const linkElement2 = document.getElementById("ar-button");
-linkElement2.addEventListener("click", function (event) {
-    if (event.target.value== "button") {
-        // Handle the user tap.
-      console.log("Ok")
-        window.open("https://www.w3schools.com");
-    }
-}, false);

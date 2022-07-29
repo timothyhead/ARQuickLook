2eda9bb0362fe3c59efb98b90d4293da701a2c2c
Timtothy head <head.timothy3@googlemail.com>
2022-07-29 14:59:25 +0100

    corrected app.js

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 
 
-const linkElement2 = document.getElementById("ar-link");
-linkElement.addEventListener("message", function (event) { ... }, false);
 
-const linkElement2 = document.getElementById("ar-link");
+const linkElement = document.getElementById("ar-link");
 linkElement.addEventListener("message", function (event) {
     if (event.data == "_apple_ar_quicklook_button_tapped") {
         // Handle the user tap.
-      window.open("https://www.w3schools.com");
+        window.open("https://www.w3schools.com");
     }
 }, false);

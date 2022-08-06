55fc57f8937f0773ea16209fe8a13085109aa7fe
Timtothy head <head.timothy3@googlemail.com>
2022-08-06 20:43:30 +0100

    added more models

--- app.js
+++ app.js
@@ -9,3 +9,21 @@ linkElement.addEventListener("message", function (event) {
         window.open("https://www.w3schools.com");
     }
 }, false);
+
+const linkElement2 = document.getElementById("CustomBanner2");
+linkElement.addEventListener("message", function (event) {
+  // window.open("https://www.w3schools.com");
+    if (event.data == "_apple_ar_quicklook_button_tapped") {
+        // Handle the user tap.
+        window.open("https://www.w3schools.com");
+    }
+}, false);
+
+const linkElement3 = document.getElementById("CustomBanner3");
+linkElement.addEventListener("message", function (event) {
+  // window.open("https://www.w3schools.com");
+    if (event.data == "_apple_ar_quicklook_button_tapped") {
+        // Handle the user tap.
+        window.open("https://www.w3schools.com");
+    }
+}, false);
